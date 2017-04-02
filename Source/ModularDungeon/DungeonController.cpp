1e090fb972fd04a63aca09138822a2afc6888732
Paul Buschmann <paul.buschmann@chello.at>
2017-04-02 13:16:05 +0200

    Major WG Bugfix and changes to module BP

--- Source/ModularDungeon/DungeonController.cpp
+++ Source/ModularDungeon/DungeonController.cpp
@@ -4,11 +4,12 @@
 #include "Kismet/KismetMathLibrary.h"
 #include "DungeonController.h"
 
+int TEST_INT = 0;
 
 // Sets default values
 ADungeonController::ADungeonController()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// Set this actor to call Tick() every frame.
 	PrimaryActorTick.bCanEverTick = false;
 }
 
@@ -24,92 +25,120 @@ void ADungeonController::BeginPlay()
 
 	if (world)
 	{
+		int attempts = 0;
 		//spawn starting module
-		AActor* startModule = GetWorld()->SpawnActor<AModule>(m_startModule, FVector(0.f, 0.f, 0.f), FRotator(0.f, 45.f, 0.f), spawnParams);
+		AActor* startModule = GetWorld()->SpawnActor<AModule>(_startModule, FVector(0.f, 0.f, 0.f), FRotator(0.f, 45.f, 0.f), spawnParams);
 
+		//get components from startmodule and paste in array
 		TArray<UChildActorComponent*> pendingExits;
 		startModule->GetComponents(pendingExits);
-		//driver
-		for (int32 x = 0; x < m_iterrations; x++)
+
+		//the dungeon algorithm
+		for (int32 x = 0; x < _iterrations; x++)
 		{
 			TArray<UChildActorComponent*> newExits;
 			for (int i = 0; i < pendingExits.Num(); i++)
 			{
-				UE_LOG(LogTemp, Warning, TEXT("pending Exits num = %i"), pendingExits.Num());
+
 				//get random tag from i-th component
 				FName tag = pendingExits[i]->ComponentTags[FMath::RandRange(0, pendingExits[i]->ComponentTags.Num() - 1)];
-				UE_LOG(LogTemp, Warning, TEXT("random tag = %s"),*tag.ToString());
 				//spawn new module at dummy location
 				FVector dummyLocation;
-				dummyLocation.Set(-1000, -1000, 0);
+				dummyLocation.Set(-100000, -100000, -100000);
 				//init to prevent error
-				AActor* newModule = GetWorld()->SpawnActor<AModule>(m_test, dummyLocation, FRotator::ZeroRotator, spawnParams);
-				newModule->Destroy();
-				//spawn correct matching module at dummy loc
-				if (tag == "corr")
+				AModule* nextModule = GetWorld()->SpawnActor<AModule>(_test, dummyLocation, FRotator::ZeroRotator, spawnParams);
+				nextModule->Destroy();
+
+				//TODO
+				// *try to place without collision to other modules
+				// *if fail try again
+				// *if success proceed
+				// *if fails try with different module for *specified times* 
+				//		or end here and mark as not connected
+
+				// *chose random module
+				//spawn module matching the random tag
+				
+				while (attempts <= _attemptsForSpawning)
 				{
-					int32 num = FMath::RandRange(0, m_corrs.Num()-1);
-					newModule = GetWorld()->SpawnActor<AModule>(m_corrs[num], dummyLocation, FRotator::ZeroRotator, spawnParams);
-					m_dungeonModules.Add(newModule);
+					if (tag == "corr")
+					{
+						int num = FMath::RandRange(0, _corrs.Num() - 1);
+						nextModule = GetWorld()->SpawnActor<AModule>(_corrs[num], dummyLocation, FRotator::ZeroRotator, spawnParams);
+					}
+					else if (tag == "junc")
+					{
+						int num = FMath::RandRange(0, _juncs.Num() - 1);
+						nextModule = GetWorld()->SpawnActor<AModule>(_juncs[num], dummyLocation, FRotator::ZeroRotator, spawnParams);
+					}
+					else if (tag == "room")
+					{
+						int num = FMath::RandRange(0, _rooms.Num() - 1);
+						nextModule = GetWorld()->SpawnActor<AModule>(_rooms[num], dummyLocation, FRotator::ZeroRotator, spawnParams);
+					}
+
+					//get new module exits
+					TArray<UChildActorComponent*> newModuleExits;
+					nextModule->GetComponents(newModuleExits);
+					//choose random exit
+					AActor* exitToMatch = newModuleExits[FMath::RandRange(0, newModuleExits.Num() - 1)]->GetChildActor();
+					//match exits
+					MatchExits(pendingExits[i]->GetChildActor(), exitToMatch);
+					if (nextModule->_isOverlappedByModule)
+					{
+						GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Magenta, TEXT("deleted overlapping module"));
+						nextModule->Destroy();
+						attempts++;
+					}
+					else
+					{
+						//get remaining unconnected exits from new module
+						for (int e = 0; e < newModuleExits.Num(); e++)
+						{
+							if (newModuleExits[e]->GetChildActor() != exitToMatch)
+								newExits.Add(newModuleExits[e]);
+						}
+						attempts++;
+						break;
+					}
 				}
-				else if (tag == "junc")
-				{
-					int32 num = FMath::RandRange(0, m_juncs.Num()-1);
-					newModule = GetWorld()->SpawnActor<AModule>(m_juncs[num], dummyLocation, FRotator::ZeroRotator, spawnParams);
-					m_dungeonModules.Add(newModule);
-				}
-				else if (tag == "room")
-				{
-					int32 num = FMath::RandRange(0, m_rooms.Num()-1);
-					newModule = GetWorld()->SpawnActor<AModule>(m_rooms[num], dummyLocation, FRotator::ZeroRotator, spawnParams);
-					m_dungeonModules.Add(newModule);
-				}
-				//get new module exits
-				TArray<UChildActorComponent*> newModuleExits;
-				newModule->GetComponents(newModuleExits);
-				//choose random exit
-				AActor* exitToMatch = newModuleExits[FMath::RandRange(0, newModuleExits.Num()-1)]->GetChildActor();
-				//match exits
-				MatchExits(pendingExits[i]->GetChildActor(), exitToMatch);
-				//get remaining unconnected exits from new module
-				for (int e = 0; e < newModuleExits.Num(); e++)
-				{
-					if (newModuleExits[e]->GetChildActor() != exitToMatch)
-						newExits.Add(newModuleExits[e]);
-				}
+
+				//add placed verified module to array
+				_dungeonModules.Add(nextModule);
+				nextModule = NULL;
 			}
+			//update pending exits
 			pendingExits = newExits;
 		}
+		//TODO
+		//* mark all unconnected exits and place *defined object*
 	}
 }
 
-void ADungeonController::MatchExits(AActor* oldE, AActor* newE)
+void ADungeonController::MatchExits(AActor* exit1, AActor* exit2)
 {
-	UE_LOG(LogTemp, Warning, TEXT("in MatchExits"));
-	AActor* newModule = newE->GetParentActor();
-	FVector forwardVectorToMatch = -oldE->GetActorForwardVector();
-	float correctiveRotation = Azimuth(forwardVectorToMatch) - Azimuth(newE->GetActorForwardVector());
+	AActor* movingModule = exit2->GetParentActor();
+	FVector forwardVectorToMatch = -exit1->GetActorForwardVector();
+	float correctiveRotation = Azimuth(forwardVectorToMatch) - Azimuth(exit2->GetActorForwardVector());
 
-	newModule->SetPivotOffset(oldE->GetActorLocation());
-	newModule->SetActorRotation(FRotator(0.f, correctiveRotation, 0.f), ETeleportType::TeleportPhysics);
-	newModule->SetPivotOffset(FVector::ZeroVector);
+	movingModule->SetPivotOffset(exit1->GetActorLocation());
+	movingModule->SetActorRotation(FRotator(0.f, correctiveRotation, 0.f), ETeleportType::TeleportPhysics);
+	movingModule->SetPivotOffset(FVector::ZeroVector);
 
-	FVector acLoc = newModule->GetActorLocation();
-	FVector correctiveTranslation = oldE->GetActorLocation() - newE->GetActorLocation();
-	newModule->SetActorLocation(acLoc + correctiveTranslation);
+	FVector acLoc = movingModule->GetActorLocation();
+	FVector correctiveTranslation = exit1->GetActorLocation() - exit2->GetActorLocation();
+	movingModule->SetActorLocation(acLoc + correctiveTranslation);
+	movingModule = NULL;
 }
 
 float ADungeonController::Azimuth(FVector vec)
 {
-	UE_LOG(LogTemp, Warning, TEXT("vecY = %.10f"), vec.Y);
-	float angle = AngleBetween(FVector::ForwardVector, vec) * FMath::Sign<float>(vec.Y);
-	return angle;
+	return AngleBetween(FVector::ForwardVector, vec) * FMath::Sign<float>(vec.Y);
 }
 
 float ADungeonController::AngleBetween(FVector from, FVector to)
 {
 	from.Normalize();
 	to.Normalize();
-	float angle = UKismetMathLibrary::DegAcos(FVector::DotProduct(from, to));
-	return angle;
+	return UKismetMathLibrary::DegAcos(FVector::DotProduct(from, to));
 }
--- Source/ModularDungeon/Module.h
+++ Source/ModularDungeon/Module.h
@@ -3,22 +3,33 @@
 #pragma once
 
 #include "GameFramework/Actor.h"
-#include "ModuleConnection.h"
 #include "Module.generated.h"
 
 UCLASS()
 class MODULARDUNGEON_API AModule : public AActor
 {
 	GENERATED_BODY()
-	
 public:
-
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* m_roomMesh;
+	// static mesh component
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UStaticMeshComponent* _mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UBoxComponent* _overlapComp;
+	class USceneComponent* _root;
+	bool _isOverlappedByModule;
 
 	AModule();
+
+	//called when something is overlapping the mesh component
+	UFUNCTION()
+	void OnModuleBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION()
+	void OnModuleEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 	virtual void BeginPlay() override;
 	
 	virtual void Tick( float DeltaSeconds ) override;
+
+	UFUNCTION(BlueprintCallable, Category="Util")
+	void SetPivotOffset(const FVector& InPivotOffset);
 };
